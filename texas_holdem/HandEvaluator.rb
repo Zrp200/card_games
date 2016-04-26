16a1cacc7709efb2efc437691da858f7f2a91966
HaysHopkins <whhopkinsiii@gmail.com>
2016-04-26 15:02:19 -0400

    Refactored and added in scoring method.

--- texas_holdem/HandEvaluator.rb
+++ texas_holdem/HandEvaluator.rb
@@ -12,7 +12,8 @@ class HandEvaluator
 
 		combinations.each do |player_cards|
 			value = get_all_card_combinations(player_cards, table) 
-			hand_value = value if value > hand_value
+			#hand_value = value if value > hand_value
+			break
 		end
 
 		#right now I'm adding all together; will eventually need to keep
@@ -46,7 +47,8 @@ class HandEvaluator
 		x, y, z = 0, 1, 2
 		hand_value = 0
 
-		#There are 10 total combinations 
+		#There are 10 total combinations
+		#Rather than 10, should have the combos be situational 
 		10.times do ||
 			hand = [player_cards[0], player_cards[1]]
 			if z == 4 && y == 3 && x == 2
@@ -87,7 +89,10 @@ class HandEvaluator
 				end
 			end
 			value =  get_hand_value(hand)
-			hand_value = value if value > hand_value
+			puts hand
+			puts value
+			puts '---------'
+			#hand_value = value if value > hand_value
 		end
 
 		hand_value
@@ -96,33 +101,38 @@ class HandEvaluator
 	def get_hand_value(hand)
 		
 		#I'll need to return the hand score and the high card
-		hand.sort! { |x,y| get_face_value(x[0]) <=> get_face_value(y[0])}
+		hand.sort!{ |x,y| get_face_value(x[0]) <=> get_face_value(y[0])}
 		high_card = hand[4]
-		faces, suits = []
+		faces, suits = split_hand(hand) 
 		
-		hand.each { |card|
-		face, suit = card.split('') 
-			faces << face; suits << suit; 
-		}
+		multiples_evaluation = check_for_multiples(faces)
+		score(multiples_evaluation) unless multiples_evaluation.size < 1
+		
+		#check_for_straight(faces)
+		#score(straight)
+		#flush_evaluation = check_for_flush(suits)
 
-		#i'll need to get back a total hand value and a high card
-		check_for_multiples(faces)
-		check_for_straight(faces)
-		check_for_flush(suits)
-		
-		1
+
+	end
+
+	def split_hand(hand)
+		faces, suits = hand.map {|card| card.split('')}.transpose
 	end
 
 	def check_for_multiples(faces)
-		faces = faces.uniq
-		if hand.size == 5
-			return;
-		elsif hand.size == 4
-			get_pair()
-		elsif hand.size == 3
-			get_three_of_kind_or_two_pair()
-		elsif hand.size == 2
-			get_four_of_kind_or_full_house()
+		multiples = faces.select {|face| faces.count(face) > 1}
+		multiples.uniq!
+
+		if multiples.size == 1
+			number = faces.count(multiples[0])
+			
+		elsif multiples.size == 2
+			number = faces.count(multiples[0])
+			number2 = faces.count(multiples[1])
+			puts "hello"
+			return [multiples[0], number, multiples[1], number]
+		else
+			return []
 		end
 	end
 
@@ -134,6 +144,31 @@ class HandEvaluator
 			return -1
 		end
 	end
+
+	def score(hand, args*)
+		#Need to convert args into the card face value (in most cases)
+		case hand
+		when 0
+			args[0] * 10
+		when 1
+			(args[0] + 13) * 10 + args[1]
+		when 2
+			(args[0] + 1) * 100
+		when 3
+			args[0] * 400
+		when 4
+			5800
+		when 5
+			(1 + args[0]) * 200 + args[1]
+		when 6
+			(args[0] + 2) * 10000
+		when 7
+			1000000 + args[0]
+		else
+			args[0]
+		end	
+	end
+
 
 	def check_suit_values(suits)
 		suits.each do |suit|
@@ -151,7 +186,7 @@ end
 
 hand_evaluator = HandEvaluator.new()
 hand = ['8c', '9h']
-table = ['As', 'Jd', '6c', 'Kc', 'Tc']
+table = ['As', '8d', 'Jc', 'Kc', 'Tc']
 
 hand_evaluator.evaluate_hand(hand, table)
 
