34122f9b3ffe65ff6dc2af2c005584db9d655894
HaysHopkins <whhopkinsiii@gmail.com>
2016-04-27 15:31:05 -0400

    Everything ready in HandEvaluator -- just need to pass right values to score(), implement calculations, and return right values at the top; also deleted Computer.

--- texas_holdem/HandEvaluator.rb
+++ texas_holdem/HandEvaluator.rb
@@ -3,34 +3,18 @@ require_relative '../Cards.rb'
 class HandEvaluator
 	include Cards
 
-	def evaluate_hand(hand, table)
-		
+	def evaluate_hand(hand, table)	
 		suits = []
 		faces = []
 		combinations = [[hand[0]], [hand[1]], hand]
 		hand_value = get_hand_value(table)
 
 		combinations.each do |player_cards|
+			#Return hand_value and hand
 			value = get_all_card_combinations(player_cards, table) 
 			#hand_value = value if value > hand_value
 			break
 		end
-
-		#right now I'm adding all together; will eventually need to keep
-		#seperate to make sure I try out all combos with player hand
-		#hand.map { |card| 
-		#	face, suit = card.split('') 
-		#	faces << face; suits << suit; 
-		#}
-		
-		#table.map { |card| 
-		#	face, suit = card.split('') 
-		#	faces << face; suits << suit;
-		#}
-
-
-		#check_suit_values(suits)
-		#check_face_values(faces)
 	end
 
 	def get_all_card_combinations(player_cards, table)
@@ -89,7 +73,7 @@ class HandEvaluator
 				end
 			end
 			value =  get_hand_value(hand)
-			puts hand
+			#puts hand
 			puts value
 			puts '---------'
 			#hand_value = value if value > hand_value
@@ -100,19 +84,15 @@ class HandEvaluator
 
 	def get_hand_value(hand)
 		
-		#I'll need to return the hand score and the high card
 		hand.sort!{ |x,y| get_face_value(x[0]) <=> get_face_value(y[0])}
-		high_card = hand[4]
 		faces, suits = split_hand(hand) 
+		get_face_values(faces)
+		get_suit_values(suits)
 		
 		multiples_evaluation = check_for_multiples(faces)
-		score(multiples_evaluation) unless multiples_evaluation.size < 1
+		flush_evaluation = check_for_flush(suits.uniq)
+		straight_evaluation = check_for_straight(faces)
 		
-		#check_for_straight(faces)
-		#score(straight)
-		#flush_evaluation = check_for_flush(suits)
-
-
 	end
 
 	def split_hand(hand)
@@ -120,81 +100,102 @@ class HandEvaluator
 	end
 
 	def check_for_multiples(faces)
+		score = -1
 		multiples = faces.select {|face| faces.count(face) > 1}
 		multiples.uniq!
-
+		
 		if multiples.size == 1
-			number = faces.count(multiples[0])
-			
+			case number = faces.count(multiples[0])
+			when 2
+				score("Pair")
+			when 3
+				score("Three of a Kind")
+			when 4
+				score("Four of a Kind")
+			else
+				"You should never be here!"
+			end
 		elsif multiples.size == 2
 			number = faces.count(multiples[0])
 			number2 = faces.count(multiples[1])
-			puts "hello"
-			return [multiples[0], number, multiples[1], number]
+			if number == 2 && number2 == 3
+				score("Full House")
+			elsif number == 3 && number2 == 2
+				score("Full House")
+			else
+				score("Two Pair")
+			end
 		else
-			return []
+			-1
 		end
 	end
 
 	def check_for_flush(suits)
-		suits = suits.uniq
 		if suits.size == 1
-			return
+			score("Flush", suits[0])
 		else
-			return -1
+			-1
 		end
 	end
 
-	def score(hand, args*)
+	def check_for_straight(faces)
+		if faces.sort.each_cons(2).all? { |x,y| y == x + 1 }
+			score("Straight", faces[4])
+		else
+			-1
+		end
+	end
+
+	def score(hand, *args)
 		#Need to convert args into the card face value (in most cases)
 		case hand
 		when "Pair"
 			#pair = (card_value * 10) Range: 20-140
 			#args[0] * 10
+			puts "Score: pair"
 		when "Two Pair"
 			#two_pair = ((13 + higher_pair_value) * 10 + second_pair_card_value) Range: 152-284
 			#(args[0] + 13) * 10 + args[1]
+			puts "Score: two pair"
 		when "Three of a Kind"
 			#three_of_kind = ((1 + card_value) * 100) Range: 300-1500
 			#(args[0] + 1) * 100
+			puts "Score: three"
 		when "Straight"
 			#args[0] * 400
+			puts "Score: straight"
 		when "Flush"
 			#flush = (5800) Range: 5800
 			#5800
+			puts "Score: flush"
 		when "Full House"
 			#full_house = (2 * three_of_a_kind_score * 10 + second_pair_card_value) Range: 6003-30013
 			#(1 + args[0]) * 200 + args[1]
+			puts "Score: full"
 		when "Four of a Kind"
 			#four_of_kind = ((2 + card_value) * 10000) Range: 40000 - 160000
 			#(args[0] + 2) * 10000
-		when "Straight Flush"
-			#straight_flush = 1000000 + high_card_value
-			#1000000 + args[0]
+			puts "Score: four" 
 		else
 			#high_card = card_value
 			#args[0]
+			puts "You should never be here!"
 		end	
 	end
 
-
-	def check_suit_values(suits)
-		suits.each do |suit|
-			puts get_suit_value(suit)
-		end
+	def get_suit_values(suits)
+		suits.map! {|suit| suit = get_suit_value(suit)}
 	end
 
-	def check_face_values(faces)
-		faces.each do |face|
-			puts get_face_value(face)
-		end
+	def get_face_values(faces)
+		faces.map! {|face| get_face_value(face)}
 	end
 
 end
 
 hand_evaluator = HandEvaluator.new()
 hand = ['8c', '9h']
-table = ['As', '8d', 'Jc', 'Kc', 'Tc']
+table = ['6s', '8c', '9c', '8c', 'Tc']
 
 hand_evaluator.evaluate_hand(hand, table)
 
